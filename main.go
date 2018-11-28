378c7646b120ad6004eecb0a93bccfed383dbc66
Soveu <panrosolek@gmail.com>
2018-11-28 19:08:58 +0100

    init commit

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/spy16/parens"
+)
+
+// Variables used for command line parameters
+var (
+	Token	string
+	lisp	*parens.Interpreter
+	root	*parens.Interpreter
+)
+
+func init() {
+
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+	scn := initScope()
+	lisp = parens.New(scn)
+	scr := parens.NewScope(scn)
+	scr.Bind("", "yes")
+	root = parens.New(scr)
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the autenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	var(
+		val	interface{}
+		err	error
+	)
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if m.Content[0] != '('{
+		return
+	}
+	if m.Content[:6] == "(sudo " { 
+		if !isSudoer(m.Author.ID){
+			s.ChannelMessageSend(m.ChannelID, "User not in sudoers file. This incident will be reported")
+			return
+		}
+		val, err = root.Execute(m.Content[6:len(m.Content)-1])
+	} else {
+		err = s.ChannelTyping(m.ChannelID)
+		val, err = lisp.Execute(m.Content)
+	}
+
+	if err != nil{
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("An error occured:```\n%s```\n", err))
+		return
+	}
+
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprint(val))
+}
+
+func isSudoer(name string) bool {
+	return name == "268379122651103233"
+}
+
---
+++ scope.go
@@ -0,0 +1,119 @@
+﻿package main
+
+import(
+	"fmt"
+	"math"
+	"errors"
+	"time"
+
+	"github.com/spy16/parens"
+)
+
+func initScope() (scope *parens.Scope){
+	scope = parens.NewScope(nil)
+	//basicScope(s)
+	//mathScope(s)
+
+	scope.Bind("ping", func() string{
+		return "pong"
+	})
+	scope.Bind("pong", func() string{
+		return "ping"
+	})
+	scope.Bind("say", func(args ...interface{}) string{
+		return fmt.Sprint(args...)
+	})
+	scope.Bind("sleep", func(t float64) error{
+		if(t > 30){
+			return errors.New("I cannot sleep THAT long!")
+		}
+		time.Sleep(time.Duration(t) * time.Second)
+		return nil
+	})
+
+	scope.Bind("+", Addition)
+	scope.Bind("-", Subtraction)
+	scope.Bind("*", Multiplication)
+	scope.Bind("/", Division)
+	scope.Bind("pow", math.Pow)
+	scope.Bind("sqrt", math.Sqrt)
+	scope.Bind("cbrt", math.Cbrt)
+	scope.Bind("sin", math.Sin)
+	scope.Bind("cos", math.Cos)
+	scope.Bind("abs", math.Abs)
+
+	scope.Bind("nan", math.NaN())
+
+	scope.Bind("e", math.E)
+	scope.Bind("pi", math.Pi)
+	scope.Bind("phi", math.Phi)
+
+	return
+}
+
+//func basicScope(scope *parser.Scope){
+//	scope.Bind("ping", func() string{
+//		return "pong"
+//	})
+//	scope.Bind("pong", func() string{
+//		return "ping"
+//	})
+//	scope.Bind("say", func(args ...interface{}) string{
+//		return fmt.Sprint(args...)
+//	})
+//	scope.Bind("sleep", func(t float64) error{
+//		if(t > 30){
+//			return errors.New("I cannot sleep THAT long!")
+//		}
+//		time.Sleep(time.Duration(t) * time.Second)
+//		return nil
+//	})
+//}
+//
+//func mathScope(scope *parens.Scope){
+//	scope.Bind("+", Addition)
+//	scope.Bind("-", Subtraction)
+//	scope.Bind("*", Multiplication)
+//	scope.Bind("/", Division)
+//	scope.Bind("pow", math.Pow)
+//	scope.Bind("sqrt", math.Sqrt)
+//	scope.Bind("cbrt", math.Cbrt)
+//	scope.Bind("sin", math.Sin)
+//	scope.Bind("cos", math.Cos)
+//
+//	scope.Bind("e", math.E)
+//	scope.Bind("pi", math.Pi)
+//}
+
+func Addition(nums... float64) (res float64){
+	for _, num := range nums{
+		res += num
+	}
+	return
+}
+
+func Subtraction(nums... float64) (res float64){
+	res = nums[0]
+	for _, num := range nums[1:]{
+		res -= num
+	}
+	return
+}
+
+func Multiplication(nums... float64) (res float64){
+	res = 1.0;
+	for _, num := range nums{
+		res = res * num
+	}
+	return
+}
+
+func Division(nums... float64) (res float64){
+	res = nums[0]
+	for _, num := range nums[1:]{
+		res = res / num
+	}
+	return
+}
+
+
